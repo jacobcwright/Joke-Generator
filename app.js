2f7578e6f7a3b1a25c012e552261492f45f23c67
Jacob Wright <36747872+jacobcwright@users.noreply.github.com>
2018-04-12 15:49:04 -0600

    Updated Code

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 $(document).ready( function() {
+    $.ajax({
+        url: 'https://icanhazdadjoke.com/',
+        method: 'GET',
+        dataType: 'JSON' 
+    }).done( function(response) {
+        $('#jokeDisplay').text(response.joke)
+    }).fail( function(error) {
+        alert(error.responseJSON.errors)
+    })
+    
     $('#nextJoke').on('click', function() {
         $.ajax({
             url: 'https://icanhazdadjoke.com/',
